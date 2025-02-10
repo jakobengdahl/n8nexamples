6df8d701c4be6cff5a030b02043b1ff46088f522
Jakob Engdahl <jakobengdahl@gmail.com>
2025-02-10 21:00:51 +0000

    added structure for more integrations

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
-#install n8n
+#!/bin/bash
+
+# Install NVM and Node.js 18
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
 nvm install 18
 nvm use 18
-npm install -g n8n
-n8n start
 
-#add API-keys
+# Install n8n
+npm install -g n8n
+
+# Generate credentials.json from environment variables
 cat <<EOF > credentials.json
 [{
   "name": "OpenAi account",
@@ -21,8 +24,18 @@ cat <<EOF > credentials.json
   "data": {
     "apiKey": "$SERP_API_KEY"
   }
+},
+{
+  "name": "Discord Webhook account",
+  "type": "discordWebhookApi",
+  "data": {
+    "webhookUri": "$DISCORD_WEBHOOK_URI"
+  }
 }]
 EOF
 
+# Import credentials into n8n
 n8n import:credentials --input=credentials.json --overwrite=true
 
+# Start n8n
+n8n start
